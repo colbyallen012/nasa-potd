0ee6ae292370486f44fb02c52e270b1be4c64e00
Colby Allen <colbyallen012@gmail.com>
2019-09-15 16:30:01 -0600

    initial commit, setup two components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header/>
+    <Potd />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import Potd from './components/Potd.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Header,
+    Potd
   }
 }
 </script>
---
+++ src/components/Potd.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Potd'
+}
+</script>
+
+<style scoped>
+
+</style>
