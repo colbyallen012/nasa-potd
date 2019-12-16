4aa9ffcf61971644b878268dcedb730e3791f30f
Colby Allen <colbyallen012@gmail.com>
2019-12-16 11:16:35 -0700

    media queries

--- src/components/Potd.vue
+++ src/components/Potd.vue
@@ -80,4 +80,45 @@ export default {
     font-size: 12px;
     font-weight: bold;
   }
+
+@media (max-width: 414px) {
+    #photoContainer {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+  }
+
+    .potd {
+    width: 95%;
+    height: 225px;
+    margin: auto;
+  }
+
+    p {
+    font-weight: bold;
+    font-size: 15px;
+    padding: 5px;
+    margin: 5px auto;
+    width: 92%;
+  }
+}
+
+@media (max-width: 1064px) {
+  #photoContainer {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(101, 114, 127, .15);
+  }
+
+  .potd {
+    width: 85%;
+    height: 325px;
+    margin: auto;
+  }
+}
+
+
 </style>
